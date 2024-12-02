65f348b111cb2b1c2b4fae4d7bc1cad2db63da00
yashGoyal40 <yashg4118@gmail.com>
2024-12-03 00:00:22 +0530

    bug-fix

--- src/pages/dashboardLayout.jsx
+++ src/pages/dashboardLayout.jsx
@@ -1,35 +1,34 @@
 import SidebarComponent from "./sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { checkLoggedInAction } from "@/actions/authAction";
 import { isLoggedIn } from "@/store/AuthSlice";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function DashboardLayout() {
   const dispatch = useDispatch();
-  const LoggedIn = useSelector(isLoggedIn)
+  const LoggedIn = useSelector(isLoggedIn);
   const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(checkLoggedInAction());
-  }, [dispatch]);
+
+    if (!LoggedIn) {
+      navigate("/auth/login");
+    }
+  }, [dispatch, LoggedIn, navigate]);
+
+
 
   return (
     <SidebarProvider>
       <div className="flex h-screen overflow-hidden">
-        {LoggedIn ? (
-          <>
-            <SidebarComponent />
-            <main className="flex-1 overflow-y-auto">
-              <Outlet />
-            </main>
-          </>
-        ) : (
-          navigate("/auth/login")
-        )}
+        <SidebarComponent />
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
       </div>
     </SidebarProvider>
   );
