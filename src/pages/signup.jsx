bbe9502d8629841442e690b80192a59179ce6d7a
yashGoyal40 <yashg4118@gmail.com>
2024-12-03 11:26:27 +0530

    file

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -266,7 +266,7 @@ export default function SignupPage() {
                 alt="Google Icon"
                 className="w-5 h-5 mr-2"
               />
-              Signup with Google
+              SignUp with Google
             </Button>
           </div>
         </CardFooter>
